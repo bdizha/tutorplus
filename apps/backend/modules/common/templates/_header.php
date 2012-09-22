37e8afb446a6bee1ff2465fcbf9e568e9a9ace25
Batanayi Matuku <bdizha@gmail.com>
2012-09-22 09:44:44 +0200

    Added more images

---
+++ apps/backend/modules/common/templates/_header.php
@@ -0,0 +1,25 @@
+<div id="inner-header-wrapper">
+    <div id="logo-container">
+        <div id="logo-wrapper">
+            <div id="header-links">                        
+                <ul>
+                    <li>
+                        <a href="/backend.php/profile">Batanayi Matuku</a>    
+                    </li>
+                    <li>
+                        <a href="#">Inbox</a>            
+                    </li>
+                    <li>
+                        <a href="#">Announcements</a>        
+                    </li>
+                    <li>
+                        <a href="#">Support</a>        
+                    </li>
+                    <li>
+                        <input class="button" value="Sign out" type="button" onclick="document.location.href='/backend.php/logout';" />
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>

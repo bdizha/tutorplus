95313ec08bd73dedb422ff8562cd7da5c03b34aa
Batanayi Matuku <bdizha@gmail.com>
2012-10-16 20:56:21 +0200

    Disabled security to be able to add new users

--- apps/backend/modules/common/templates/_footer.php
+++ apps/backend/modules/common/templates/_footer.php
@@ -2,11 +2,11 @@
     <div id="copyright">TutorPlus.org &copy; <?php echo date("Y") ?>. All rights reserved.</div>
     <div id="footer-links">
         <ul>
-            <li><a href="http://tutorplus.org/support" class="first" target="_blank">Support</a></li>
-            <li><a href="http://tutorplus.org/terms" target="_blank">Terms of service</a></li>
-            <li><a href="http://tutorplus.org/privacy-policy" target="_blank">Privacy policy</a></li>
-            <li><a href="http://www.facebook.com/tutorplus" target="_blank">Facebook</a></li>
-            <li><a href="http://www.twitter.com/tutorplus" target="_blank">Twitter</a></li>
+            <li><a href="#" class="first" target="_blank">Support</a></li>
+            <li><a href="#" target="_blank">Terms of service</a></li>
+            <li><a href="#" target="_blank">Privacy policy</a></li>
+            <li><a href="#" target="_blank">Facebook</a></li>
+            <li><a href="#" target="_blank">Twitter</a></li>
         </ul>                        
     </div>
     <div id="ownership">Powered by <a target="_blank" href="http://www.graphifox.com">www.graphifox.com</a></div>

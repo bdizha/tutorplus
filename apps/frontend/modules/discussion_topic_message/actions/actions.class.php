6df2bc900ec1c0907806f200e73b8b74e0d931c5
Batanayi <bdizha@gmail.com>
2012-11-11 17:00:31 +0200

    Added new email notification on each newly created discussion topic

--- apps/frontend/modules/discussion_topic_message/actions/actions.class.php
+++ apps/frontend/modules/discussion_topic_message/actions/actions.class.php
@@ -11,40 +11,32 @@ require_once dirname(__FILE__) . '/../lib/discussion_topic_messageGeneratorHelpe
  * @author     Batanayi Matuku
  * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
  */
-class discussion_topic_messageActions extends autoDiscussion_topic_messageActions
-{
+class discussion_topic_messageActions extends autoDiscussion_topic_messageActions {
 
-    public function executeShow(sfWebRequest $request)
-    {
-        $discussionTopicMessageId = $this->getUser()->getMyAttribute('discussion_topic_message_show_id', null);        
-        $this->discussionTopicMessage = DiscussionTopicMessageTable::getInstance()->find($this->getUser()->getMyAttribute('discussion_topic_message_show_id', null));
-        
-        if(!$this->discussionTopicMessage){
+    public function executeShow(sfWebRequest $request) {
+        $discussionTopicMessageId = $this->getUser()->getMyAttribute('discussion_topic_message_show_id', null);
+        $this->discussionTopicMessage = DiscussionTopicMessageTable::getInstance()->find($discussionTopicMessageId);
+
+        if (!$this->discussionTopicMessage) {
             die();
-        }
-        else{
+        } else {
             $this->replyForm = new DiscussionTopicReplyForm();
         }
     }
 
-    protected function processForm(sfWebRequest $request, sfForm $form)
-    {
+    protected function processForm(sfWebRequest $request, sfForm $form) {
         $form->bind($request->getParameter($form->getName()), $request->getFiles($form->getName()));
-        if ($form->isValid())
-        {
+        if ($form->isValid()) {
+            $isNew = $form->getObject()->isNew();
             $notice = $form->getObject()->isNew() ? 'Your message was created successfully.' : 'Your message was updated successfully.';
 
-            try
-            {
+            try {
                 $discussion_topic_message = $form->save();
-            }
-            catch (Doctrine_Validator_Exception $e)
-            {
+            } catch (Doctrine_Validator_Exception $e) {
                 $errorStack = $form->getObject()->getErrorStack();
 
                 $message = get_class($form->getObject()) . ' has ' . count($errorStack) . " field" . (count($errorStack) > 1 ? 's' : null) . " with validation errors: ";
-                foreach ($errorStack as $field => $errors)
-                {
+                foreach ($errorStack as $field => $errors) {
                     $message .= "$field (" . implode(", ", $errors) . "), ";
                 }
                 $message = trim($message, ', ');
@@ -52,17 +44,37 @@ class discussion_topic_messageActions extends autoDiscussion_topic_messageAction
                 $this->getUser()->setFlash('error', $message);
                 return sfView::SUCCESS;
             }
-            
+
+            // send the discussion_topic_message emails
+            if ($isNew) {
+                $this->sendEmail($discussion_topic_message);
+            }
+
             // session the new created discussion topic message id
             $this->getUser()->setMyAttribute('discussion_topic_message_show_id', $discussion_topic_message->getId());
 
             $this->getUser()->setFlash('notice', $notice);
             die("success");
-        }
-        else
-        {
+        } else {
             $this->getUser()->setFlash('error', 'The item has not been saved due to some errors.', false);
         }
+    }
+
+    public function sendEmail($object) {
+        $toEmails = $object->getToEmails();
+        $owner = $object->getUser();
+        $mailer = new tpMailer();
+        $mailer->setTemplate('new-discussion-topic-message');
+        $mailer->setToEmails($toEmails);
+        $mailer->addValues(array(
+            "OWNER" => $owner->getName(),
+            "DISCUSSION_TOPIC_MESSAGE" => $object->getMessage(),
+            "DISCUSSION_TOPIC_LINK" => $this->getPartial('email_template/link', array(
+                'title' => $this->generateUrl('discussion_topic_message_show', array("slug" => $object->getDiscussionTopic()->getSlug()), 'absolute=true'),
+                'route' => "@discussion_topic_message_show?slug=" . $object->getDiscussionTopic()->getSlug())
+                )));
+
+        $mailer->send();
     }
 
 }
--- apps/frontend/modules/discussion_topic_reply/actions/actions.class.php
+++ apps/frontend/modules/discussion_topic_reply/actions/actions.class.php
@@ -11,20 +11,16 @@ require_once dirname(__FILE__) . '/../lib/discussion_topic_replyGeneratorHelper.
  * @author     Batanayi Matuku
  * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
  */
-class discussion_topic_replyActions extends autoDiscussion_topic_replyActions
-{
+class discussion_topic_replyActions extends autoDiscussion_topic_replyActions {
 
-    public function executeShow(sfWebRequest $request)
-    {
+    public function executeShow(sfWebRequest $request) {
         $this->discussionTopicMessageReply = $this->getRoute()->getObject();
-        if (!$this->discussionTopicMessageReply)
-        {
+        if (!$this->discussionTopicMessageReply) {
             die();
         }
     }
 
-    public function executeCreate(sfWebRequest $request)
-    {
+    public function executeCreate(sfWebRequest $request) {
         $this->form = $this->configuration->getForm();
         $this->discussion_topic_reply = $this->form->getObject();
 
@@ -32,42 +28,57 @@ class discussion_topic_replyActions extends autoDiscussion_topic_replyActions
         $this->setTemplate('new');
     }
 
-    public function executeNew(sfWebRequest $request)
-    {
+    public function executeNew(sfWebRequest $request) {
         $this->form = $this->configuration->getForm();
 
-        if ($request->getMethod() == "POST")
-        {
+        if ($request->getMethod() == "POST") {
             $this->processForm($request, $this->form);
-        }
-        else
-        {
+        } else {
             $discussionTopicMessageId = $this->getUser()->getMyAttribute('discussion_topic_message_show_id', null);
             $this->discussionTopicMessage = DiscussionTopicMessageTable::getInstance()->find($discussionTopicMessageId);
         }
     }
 
-    protected function processForm(sfWebRequest $request, sfForm $form)
-    {
+    public function sendEmail($object) {
+        $toEmails = $object->getToEmails();
+        $owner = $object->getUser();
+        $mailer = new tpMailer();
+        $mailer->setTemplate('new-discussion-topic-reply');
+        $mailer->setToEmails($toEmails);
+        $mailer->addValues(array(
+            "OWNER" => $owner->getName(),
+            "DISCUSSION_TOPIC_REPLY" => $object->getMessage(),
+            "DISCUSSION_TOPIC_LINK" => $this->getPartial('email_template/link', array(
+                'title' => $this->generateUrl('discussion_topic_message_show', array("slug" => $object->getDiscussionTopicMessage()->getDiscussionTopic()->getSlug()), 'absolute=true'),
+                'route' => "@discussion_topic_message_show?slug=" . $object->getDiscussionTopicMessage()->getDiscussionTopic()->getSlug())
+                )));
+
+        $mailer->send();
+    }
+
+    protected function processForm(sfWebRequest $request, sfForm $form) {
         $form->bind($request->getParameter($form->getName()), $request->getFiles($form->getName()));
-        if ($form->isValid())
-        {
-            try
-            {
+        if ($form->isValid()) {
+            try {
+                $isNew = $form->getObject()->isNew();
+
                 $this->discussion_topic_reply = $form->save();
                 $this->getUser()->setMyAttribute('discussion_topic_message_show_id', $this->discussion_topic_reply->getDiscussionTopicMessageId());
+
+
+                // send the discussion_topic_message emails
+                if ($isNew) {
+                    $this->sendEmail($discussion_topic_message);
+                }
+
                 echo $this->discussion_topic_reply->getId();
                 exit;
-            }
-            catch (Doctrine_Validator_Exception $e)
-            {
+            } catch (Doctrine_Validator_Exception $e) {
                 
             }
 
             $this->dispatcher->notify(new sfEvent($this, 'admin.save_object', array('object' => $this->discussion_topic_reply)));
-        }
-        else
-        {
+        } else {
             die("failure");
         }
     }

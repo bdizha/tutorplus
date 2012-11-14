4110246ea25999dd2097d6ebd7a08166c6da8c5e
Batanayi <bdizha@gmail.com>
2012-11-14 20:41:25 +0200

    Modified the home page images and student enroll modules

---
+++ apps/frontend/modules/student_enroll/templates/_courses_form_fieldset.php
@@ -0,0 +1,17 @@
+<?php foreach (DepartmentTable::getInstance()->findAll() as $department): ?>
+    <?php if ($department->getCourses()->count() > 0): ?>
+        <fieldset id="sf_fieldset_<?php echo preg_replace('/[^a-z0-9_]/', '_', strtolower($department->getName())) ?>">
+            <h2><?php echo $department->getName() ?> (<?php echo $department->getAbbreviation() ?>)</h2>
+            <?php foreach ($department->getCourses() as $course): ?>
+                <?php $name = preg_replace('/[^a-z0-9_]/', '_', strtolower($course->getCode())) ?>
+                <?php $label = $course->getName() ?>
+                <div class="department-course sf_admin_form_field_<?php echo $name ?>"> 
+                    <div class="checkbox">
+                        <?php echo $form[$name]->render() ?>
+                    </div>   
+                    <?php echo $form[$name]->renderLabel($label) ?>
+                </div>
+            <?php endforeach; ?>
+        </fieldset>
+    <?php endif; ?>
+<?php endforeach; ?>

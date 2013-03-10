cb8586fa4eacbe9f1d921342df4ae2ed8b9a4dd2
Batanayi <bdizha@gmail.com>
2013-03-10 16:44:20 +0200

    Removed the delete button from the new topic page

--- apps/frontend/modules/discussion/templates/newSuccess.php
+++ apps/frontend/modules/discussion/templates/newSuccess.php
@@ -3,15 +3,15 @@
 <?php include_partial('common/breadcrumbs', $helper->newBreadcrumbs($form)) ?>
 <?php include_partial('common/flashes_normal', array('form' => $form)) ?>
 <div id="sf_admin_form_container">
-	<div id="sf_admin_content">
-		<div class="content-block">
-			<?php include_partial('common/tabs', array('tabs' => $helper->getTabs($myDiscussions, $exploreDiscussions, "new"))) ?>
-			<div class="tab-block">
-				<?php include_partial('discussion/form', array('discussion' => $discussion, 'form' => $form, 'configuration' => $configuration, 'helper' => $helper)) ?>
-			</div>
-		</div>
-		<div id="sf_admin_footer">
-			<?php include_partial('discussion/form_footer', array('discussion' => $discussion, 'form' => $form, 'configuration' => $configuration)) ?>
-		</div>
-	</div>
+    <div id="sf_admin_content">
+        <div class="content-block">
+            <?php include_partial('common/tabs', array('tabs' => $helper->getTabs($myDiscussions, $exploreDiscussions, "new"))) ?>
+            <div class="tab-block">
+                <?php include_partial('discussion/form', array('discussion' => $discussion, 'form' => $form, 'configuration' => $configuration, 'helper' => $helper)) ?>
+            </div>
+        </div>
+        <div id="sf_admin_footer">
+            <?php include_partial('discussion/form_footer', array('discussion' => $discussion, 'form' => $form, 'configuration' => $configuration)) ?>
+        </div>
+    </div>
 </div>
